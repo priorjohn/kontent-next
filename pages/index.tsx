defdd3266ce2f5a40a22efe79a41d97e8ae6fdd9
priorjohn <75687824+priorjohn@users.noreply.github.com>
2023-01-10 16:53:28 +0000

    Uniform

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,13 @@ import styles from '../styles/Home.module.css'
 import { getHeroUnit } from "../lib/kontentClient";
 import { GetStaticProps, NextPage } from 'next';
 import { HeroUnit } from '../models/content-types/hero_unit';
+import { CanvasClient } from "@uniformdev/canvas";
+import { Composition } from "@uniformdev/canvas-react";
+import LayoutCanvas from "../src/components/LayoutCanvas";
+
+import doEnhance from "../lib/enhancer";
+import resolveRenderer from "../lib/resolveRenderer";
+
 
 const Home: NextPage<IndexProps> = ({ heroUnit }) => {
   return (
@@ -11,11 +18,13 @@ const Home: NextPage<IndexProps> = ({ heroUnit }) => {
         <div className={styles.summary} dangerouslySetInnerHTML={{ __html: heroUnit.elements.marketingMessage.value }}>
         </div>
       </div>
+      <Composition data={composition} resolveRenderer={resolveRenderer}>
+        <LayoutCanvas composition={composition} fields={fields} />
+      </Composition>
     </main>
   )
 }
 
-export default Home
 
 interface IndexProps {
   heroUnit: HeroUnit;
@@ -24,7 +33,23 @@ interface IndexProps {
 export const getStaticProps: GetStaticProps<IndexProps> = async () => {
   const heroUnit = await getHeroUnit();
 
+  const composition = await getComposition(slug);
+  await doEnhance(composition);
+
   return {
-    props: { heroUnit },
+    props: { heroUnit,
+          composition,
+         fields: topic.fields, },
   };
+}
+
+async function getComposition(slug) {
+  const client = new CanvasClient({
+    apiKey: process.env.UNIFORM_API_KEY,
+    projectId: process.env.UNIFORM_PROJECT_ID,
+  });
+  const { composition } = await client.getCompositionBySlug({
+    slug,
+  });
+  return composition;
 }
